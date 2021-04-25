7b7786a78e8f5a1b6f0f0f7e2151601a9562b3af
rmdhani0812@gmail.com <rmdhani0812@gmail.com>
2021-04-25 13:09:28 +0700

    If Else Percabangan

---
+++ src/Pencabangan/IfElse.java
@@ -0,0 +1,15 @@
+package Pencabangan;
+
+public class IfElse {
+    public static void main(String[] args) {
+        double totalBelanja = 288000 ;
+        double uangDiDompet = 500000 ;
+
+        if (uangDiDompet<totalBelanja){
+            System.out.println("Uang Kurang, Kurangi Jajan Anda");
+        }else{
+            System.out.println("Uang Cukup, Selamat Menikmati");
+        }
+
+    }
+}
