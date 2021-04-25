76ad43063ec13ed9ed8746e56c8326b053199a91
rmdhani0812@gmail.com <rmdhani0812@gmail.com>
2021-04-25 12:11:37 +0700

    Operaator Binery Arimatika

---
+++ src/Operator/OperatorBinary.java
@@ -0,0 +1,18 @@
+package Operator;
+
+public class OperatorBinary {
+    String nama = "AHMAD RAHMADHANI";
+    String NPM  = "19630327";
+    String Kelas= "4A NON REG BJB";
+    int jumlahSKS = 24;
+    double IndeksPresatasiKumulatif = 3.89;
+
+    public static void main(String[] args) {
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9/3);
+        System.out.println(9*3);
+        System.out.println(9%3);
+        System.out.println(9%2);
+    }
+}
